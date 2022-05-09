c18f1c1532f30443524a59166651df3ae03aafcf
reddington12 <37213559+Reddington12@users.noreply.github.com>
2022-05-09 09:53:51 +0200

    pagination-table

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,52 @@
-import React from "react";
 import { StyledArticle } from "./style/Article.styled";
 import Table from "./Table";
+import React, { useMemo, useState, useEffect } from "react";
+
+import axios from "axios";
 
 function Article() {
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Header",
+        columns: [
+          {
+            Header: "Language",
+            accessor: "serialNum",
+          },
+          {
+            Header: "Genre(s)",
+            accessor: "studName",
+          },
+          {
+            Header: "Runtime",
+            accessor: "location",
+          },
+          {
+            Header: "Status",
+            accessor: "date",
+          },
+          {
+            Header: "Language",
+            accessor: "compType",
+          },
+        ],
+      },
+    ],
+    []
+  );
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const result = await axios("http://localhost:3004/assets");
+      setData(result.data);
+    })();
+  }, []);
   return (
     <StyledArticle>
-      <Table></Table>
+      <Table columns={columns} data={data} />
     </StyledArticle>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -6,54 +6,119 @@ import { TableStyled } from "./style/Table.styled";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-function Table() {
-  const [arrAssets, setAsset] = useState([]);
-  useEffect(() => {
-    fetchAssets();
-  }, []);
+import { useTable, useFilters, useSortBy, usePagination } from "react-table";
 
-  async function fetchAssets() {
-    try {
-      const data = await axios.get(`http://localhost:3004/assets`, {
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-      });
+export default function Table({ columns, data }) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page, // Instead of using 'rows', we'll use page,
+    // which has only the rows for the active page
 
-      console.log(data.data, "data");
-      setAsset(data.data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
+    // The rest of these things are super handy, too ;)
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 },
+    },
+    usePagination
+  );
   return (
     <Wrapper>
       <FilterBox></FilterBox>
 
-      <TableStyled>
-        <tr>
-          <th>Student name</th>
-          <th>Serialnumber</th>
-          <th>Computer type</th>
-          <th>Status</th>
-          <th>Borrowed computer</th>
-          <th>Last updated</th>
-        </tr>
-        {arrAssets.map((asset) => (
-          <tr key={asset.id}>
-            <th>{asset.studName}</th>
-            <th>{asset.serialNum}</th>
-            <th>{asset.compType}</th>
-            <th>{asset.status}</th>
-            <th>{asset.location}</th>
-            <th>{asset.date}</th>
-          </tr>
-        ))}
+      <TableStyled {...getTableProps()}>
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {page.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        {/* 
+        Pagination can be built however you'd like. 
+        This is just a very basic UI implementation:
+      */}
+        <div className="pagination">
+          <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+            {"<<"}
+          </button>{" "}
+          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+            {"<"}
+          </button>{" "}
+          <button onClick={() => nextPage()} disabled={!canNextPage}>
+            {">"}
+          </button>{" "}
+          <button
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+          >
+            {">>"}
+          </button>{" "}
+          <span>
+            Page{" "}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{" "}
+          </span>
+          <span>
+            | Go to page:{" "}
+            <input
+              type="number"
+              defaultValue={pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                gotoPage(page);
+              }}
+              style={{ width: "100px" }}
+            />
+          </span>{" "}
+          <select
+            value={pageSize}
+            onChange={(e) => {
+              setPageSize(Number(e.target.value));
+            }}
+          >
+            {[3, 10, 20, 30, 40, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
       </TableStyled>
     </Wrapper>
   );
 }
-
-export default Table;
